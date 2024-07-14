ecbb16df7c42e328ced65309dce81dbe2e040dc2
TylerOsborne <tylerosborne2018@hotmail.com>
2024-07-14 17:57:44 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cleaning-list';
+
+  // ng build --base-href "https://tyler-w-osborne.github.io/task-list/" && npx angular-cli-ghpages --dir=docs && git add . && git commit -m "changes" && git push origin main
 }
